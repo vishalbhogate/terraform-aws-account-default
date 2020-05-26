62b7cae47700dd959a44273e4acc2c72b9fda61d
vishal <vishal.bhogate07@gmail.com>
2020-05-26 15:20:08 +1000

    Revising the commits

--- _backend.tf
+++
@@ -1,9 +0,0 @@
-### Should be the part of module###
-terraform {
-  backend "s3" {
-    bucket  = "mumbai-terraform-backend"
-    key     = "network"
-    region  = "ap-south-1"
-    encrypt = true
-  }
-}
